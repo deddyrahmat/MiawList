cebd04aef4bfc2d1f980224086b16f8efa54dee9
deddyrahmat <deddyrahmat97@gmail.com>
2023-01-03 17:58:08 +0700

    create feature load more
    create reature load more limit per 10 data

--- src/components/molecules/Card/index.js
+++ src/components/molecules/Card/index.js
@@ -1,10 +1,26 @@
+import Buttons from 'components/atoms/Buttons'
 import React from 'react'
 
-function Card() {
+import './card.css'
+
+function Card({data}) {
+    // console.log('data', data)
   return (
-    <div className="border rounded">
-        tes
-    </div>
+    <>
+        <div className="border rounded-lg border-2 p-5 flex flex-col ">
+            <div className='flex-1 min-h-[180px]'>
+                <p className="text-slate-600 text-sm">{data.origin}</p>
+                <p className="text-black text-lg font-bold mt-2 font-roboto">{data.name}</p>
+                <p className='mt-1 mb-3 text-sm text-slate-400'>
+                    {data.temperament}
+                </p>
+                <p className='truncate-2'>
+                    {data.description}
+                </p>
+            </div>
+            <Buttons className='mt-3 flex-1 bg-sky-400 rounded-lg text-white py-2 text-lg'>Detail</Buttons>
+        </div>
+    </>
   )
 }
 
--- src/components/organisms/ListImages/index.js
+++ src/components/organisms/ListImages/index.js
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from 'react';
 
 import ApiCats from 'config/Endpoint/cats'
 import Card from 'components/molecules/Card';
+import Buttons from 'components/atoms/Buttons';
 
 function ListImages() {
     
     const [limit, setLimit] = useState(10);
-    const [page, setPage] = useState(1);
+    const [page, setPage] = useState(0);
     const [dataCats, setDataCats] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
@@ -27,12 +28,31 @@ function ListImages() {
 
     useEffect(() => {
         prosesList();
-    }, [page]);
+    }, [limit]);
 
-    console.log('dataCats', dataCats)
+    const handleLimit = () => {
+        setLimit(limit + 10)
+    }
+
+    // console.log('dataCats', dataCats)
   return (
     <>
-        <Card />
+        <div className="container mx-auto">
+            <div className="grid grid-cols-4 gap-4">
+                {
+                    dataCats?.length > 0 && (
+                        dataCats.map((cat, index) => 
+                            <Card data={cat} key={index}/>
+                        )
+                    )
+                }
+            </div>
+            <Buttons onClick={() => {handleLimit()}} isDisabled={isLoading} className={`mt-5 w-4/12 mx-auto block text-center flex-1 rounded-lg text-white py-2 text-xl font-bold ${isLoading ? 'bg-slate-400 pointer-events-none' : 'bg-sky-400'}`}>
+                {
+                    isLoading ? (<p>Loading...</p>) : ("Load More")
+                }
+            </Buttons>
+        </div>
     </>
   )
 }
