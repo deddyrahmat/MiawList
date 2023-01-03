91e3a59e50242caee4aac28b094705c94e455e28
deddyrahmat <deddyrahmat97@gmail.com>
2023-01-03 18:00:47 +0700

    fixing responsive grid
    fixing responsive show card in grid

--- src/components/organisms/ListImages/index.js
+++ src/components/organisms/ListImages/index.js
@@ -38,7 +38,7 @@ function ListImages() {
   return (
     <>
         <div className="container mx-auto">
-            <div className="grid grid-cols-4 gap-4">
+            <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 px-2">
                 {
                     dataCats?.length > 0 && (
                         dataCats.map((cat, index) => 
