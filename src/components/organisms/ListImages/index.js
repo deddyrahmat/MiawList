39630379aa733b5efc50c5ed4ca995c2b8d1be65
deddyrahmat <deddyrahmat97@gmail.com>
2023-01-04 08:42:32 +0700

    create component search
    cut code search in organism and create component search in molecules

--- src/components/organisms/ListImages/index.js
+++ src/components/organisms/ListImages/index.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react';
 import ApiCats from 'config/Endpoint/cats'
 import Card from 'components/molecules/Card';
 import Buttons from 'components/atoms/Buttons';
+import Search from 'components/molecules/Search';
 
 function ListImages() {
     
@@ -72,23 +73,13 @@ function ListImages() {
     // console.log('dataCats', dataCats)
   return (
     <>
-        <div className="relative w-full h-[500px]">
-
-            <img src="https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80" alt="background" className='object-cover object-center w-full h-full' />
-            <div className="container mx-auto px-2 w-full lg:w-6/12 xl:w-5/12 absolute inset-1/2 -translate-y-2/4 -translate-x-2/4 flex justify-between align-center">
-                <input type="text" className='px-2 py-1 w-10/12 mx-auto bg-white border h-12 md:rounded-lg shadow' onKeyDown={(event) => handleKeywordKeydown(event)} onChange={(event) => handleKeyword(event)} placeholder="Search name cat" />
-                <Buttons isDisabled={isLoadingSearch} className={`text-white h-12 flex items-center md:rounded-lg text-lg px-5 font-semibold shadow ${isLoadingSearch ? 'bg-slate-400 pointer-events-none' : 'bg-sky-400'}`} onClick={() => handleSearch()}>
-                    {
-                        isLoadingSearch ? ("Loading..") : ("Search")
-                    }
-                </Buttons>
-            </div>
-        </div>
-        <div className="container mx-auto my-10">
+        <Search handleKeywordKeydown={handleKeywordKeydown} handleKeyword={handleKeyword}  handleSearch={handleSearch}  isLoadingSearch={isLoadingSearch} />
+        
+        <div className="container mx-auto my-10 px-2">
             {statusSearch && (
                 <p className='text-red-600 text-xl font-bold mb-5'>{statusSearch}</p>
             )}
-            <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 px-2">
+            <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
                 {
                     dataCats?.length > 0 && (
                         dataCats.map((cat, index) => 
