4cf083ffbf1c845780cf11da068eae8b910784f5
deddyrahmat <deddyrahmat97@gmail.com>
2023-01-04 14:39:08 +0700

    fixing get api detail
    fixing with add validation length when get data in detail page

--- src/components/organisms/DetailImage/index.js
+++ src/components/organisms/DetailImage/index.js
@@ -32,7 +32,7 @@ function DetailImage() {
     useEffect(() => {
         prosesDetail();
     }, []);
-    // console.log('dataCats', dataCats)
+    console.log('dataCats', dataCats)
   return (
     <div className='border rounded-lg border-2 p-5 flex flex-col '>
         <Buttons href='/' type='link' className='mt-3 bg-sky-400 rounded-lg text-white p-2 text-lg shadow mb-10 w-6/12 md:w-5/12 lg:w-2/12'> 
@@ -45,32 +45,34 @@ function DetailImage() {
         {isLoading ? (
             <FaSpinner className='animate-spin mx-auto' size={30} />
         ) : (
-            <div className='px-2'>
-            <div className="flex space-x-3 flex-col lg:flex-row">
-                <ImageHeader id={dataCats.reference_image_id} />
-                <div>
-                    <div className="flex flex-col lg:flex-row space-x-3 text-md lg:text-xl font-bold">
-                        <p>Name :</p>
-                        <p>{dataCats.name}</p>
+            Object.keys(dataCats).length > 0 && (
+                <div className='px-2'>
+                    <div className="flex space-x-3 flex-col lg:flex-row">
+                        <ImageHeader id={dataCats.reference_image_id} />
+                        <div>
+                            <div className="flex flex-col lg:flex-row space-x-3 text-md lg:text-xl font-bold">
+                                <p>Name :</p>
+                                <p>{dataCats.name}</p>
+                            </div>
+                            <div className="flex flex-col lg:flex-row space-x-3 text-md lg:text-xl font-bold">
+                                <p>Origin :</p>
+                                <p>{dataCats.origin}</p>
+                            </div>
+                            <div className="flex flex-col lg:flex-row space-x-3 text-md lg:text-xl font-bold">
+                                <p>Temperament :</p>
+                                <p>{dataCats.temperament}</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex flex-col lg:flex-row space-x-3 text-md lg:text-xl font-bold">
-                        <p>Origin :</p>
-                        <p>{dataCats.origin}</p>
-                    </div>
-                    <div className="flex flex-col lg:flex-row space-x-3 text-md lg:text-xl font-bold">
-                        <p>Temperament :</p>
-                        <p>{dataCats.temperament}</p>
-                    </div>
-                </div>
-            </div>
-            
-            <p className='text-md lg:text-xl font-bold mt-5'>Description : </p>
-            <p className='mb-5 text-md lg:text-xl'>{dataCats.description}</p>
-            <p className='text-md lg:text-xl font-bold mt-5'>Source : </p>
-            <Buttons type="link" href={dataCats.wikipedia_url} isExternal={true} target="_blank" className="text-blue-400">- Wikipedia</Buttons>
+                    
+                    <p className='text-md lg:text-xl font-bold mt-5'>Description : </p>
+                    <p className='mb-5 text-md lg:text-xl'>{dataCats.description}</p>
+                    <p className='text-md lg:text-xl font-bold mt-5'>Source : </p>
+                    <Buttons type="link" href={dataCats.wikipedia_url} isExternal={true} target="_blank" className="text-blue-400">- Wikipedia</Buttons>
 
-            <CardList data={dataCats.id} />
-            </div>
+                    <CardList data={dataCats.id} />
+                </div>
+            )
         )}
     </div>
   )
